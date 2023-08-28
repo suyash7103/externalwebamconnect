b4d95a4b1632a8de551070004d47c8c833c9168f
suyash7103 <112558612+suyash7103@users.noreply.github.com>
2023-08-28 21:36:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/externalcam/MainActivity.kt
@@ -0,0 +1,141 @@
+package com.example.externalcam
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.hardware.camera2.*
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var cameraManager: CameraManager
+    private var cameraDevice: CameraDevice? = null
+    private lateinit var surfaceView: SurfaceView
+    private lateinit var surfaceHolder: SurfaceHolder
+    private var cameraHandlerThread: HandlerThread? = null
+    private var cameraHandler: Handler? = null
+    private var cameraCaptureSession: CameraCaptureSession? = null
+
+    private val cameraStateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            cameraDevice = camera
+            startPreview()
+        }
+
+        override fun onDisconnected(camera: CameraDevice) {
+            cameraDevice?.close()
+            cameraDevice = null
+        }
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            cameraDevice?.close()
+            cameraDevice = null
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        surfaceView = findViewById(R.id.surfaceView)
+        surfaceHolder = surfaceView.holder
+        surfaceHolder.addCallback(surfaceCallback)
+
+        cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST)
+        } else {
+            openCamera()
+        }
+
+        cameraHandlerThread = HandlerThread("CameraHandlerThread")
+        cameraHandlerThread?.start()
+        cameraHandler = Handler(cameraHandlerThread!!.looper)
+    }
+
+    private val surfaceCallback = object : SurfaceHolder.Callback {
+        override fun surfaceCreated(holder: SurfaceHolder) {
+            // Surface is created; open the camera
+            openCamera()
+        }
+
+        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+            // Surface dimensions changed; handle it here if needed
+        }
+
+        override fun surfaceDestroyed(holder: SurfaceHolder) {
+            // Surface is destroyed; release resources
+            cameraDevice?.close()
+        }
+    }
+
+    private fun openCamera() {
+        val cameraId = cameraManager.cameraIdList[0]
+        try {
+            cameraManager.openCamera(cameraId, cameraStateCallback, null)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun startPreview() {
+        val surface = surfaceHolder.surface
+
+        cameraDevice?.createCaptureSession(
+            listOf(surface),
+            object : CameraCaptureSession.StateCallback() {
+                override fun onConfigured(session: CameraCaptureSession) {
+                    cameraCaptureSession = session
+                    val captureRequestBuilder = cameraDevice?.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)?.apply {
+                        addTarget(surface)
+                    }
+                    val captureRequest = captureRequestBuilder?.build()
+
+                    captureRequest?.let {
+                        cameraCaptureSession?.setRepeatingRequest(it, null, cameraHandler)
+                    }
+                }
+
+                override fun onConfigureFailed(session: CameraCaptureSession) {
+                    // Handle configuration failure here
+                }
+            },
+            cameraHandler
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, open the camera
+                openCamera()
+            } else {
+                // Permission denied, handle this case (e.g., show a message or exit the app)
+                finish()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraCaptureSession?.close()
+        cameraHandlerThread?.quitSafely()
+        cameraDevice?.close()
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST = 100
+    }
+}
